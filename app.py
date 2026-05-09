a7a605e6bf502f8afc5278bd914b39a12df9d57b
royaniruddha404-debug <royaniruddha404@gmail.com>
2026-05-09 19:20:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ with st.sidebar:
     model_name = st.selectbox(
         "Select Model",
         [
-            "llama-3.3-70b-versatile",
             "llama-3.1-8b-instant",
+            "llama-3.3-70b-versatile",
         ]
     )
 
